4c7def2bfe61395e5fed80c4d9dc6185719f5822
Marko1310 <marko.cabo13@gmail.com>
2023-06-17 15:07:21 +0200

    authController testing complete

--- __tests__/authController.test.ts
+++ __tests__/authController.test.ts
@@ -3,6 +3,8 @@ import authController from '../controllers/authController';
 import inputController from '../controllers/inputController';
 import userServices from '../services/userServices';
 import jwtServices from '../services/jwtServices';
+import AppError from '../services/appErrorServices';
+import bcryptServices from '../services/bcryptServices';
 
 jest.mock('../controllers/inputController', () => ({
   isValidEmail: jest.fn(),
@@ -25,7 +27,16 @@ jest.mock('../services/jwtServices', () => ({
   sendJwtResponse: jest.fn(),
 }));
 
+jest.mock('../services/bcryptServices', () => ({
+  checkPassword: jest.fn(),
+}));
+
+jest.mock('bcryptjs');
+
 describe('register user', () => {
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
   it('should perform basic input checks, call userServices.newUser and jwtServices.sendJwtResponse functions with the rigth parameters', async () => {
     const req: Partial<Request> = {
       body: {
@@ -52,7 +63,6 @@ describe('register user', () => {
 
     await authController.register(req as Request, res as unknown as Response, next);
 
-    // Assertions
     expect(inputController.isValidEmail).toHaveBeenCalledWith('test@example.com');
     expect(inputController.isValidPassword).toHaveBeenCalledWith('password123');
     expect(inputController.isValidName).toHaveBeenCalledWith('John', 'First name');
@@ -60,5 +70,88 @@ describe('register user', () => {
     expect(userServices.newUser).toHaveBeenCalledWith('test@example.com', 'password123', 'John', 'Doe', 'user');
     expect(jwtServices.sendJwtResponse).toHaveBeenCalledWith(mockUserOutput, res);
     expect(next).not.toHaveBeenCalled();
+  });
+});
+
+describe('login user', () => {
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+  it('should return a 401 error if user is not found in the db and bcrypt.compare should not be called', async () => {
+    const req: Partial<Request> = {
+      body: {
+        email: 'test@example.com',
+        password: 'password123',
+      },
+    };
+    const res: Partial<Response> = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+    const next = jest.fn();
+
+    const mockError = new AppError('Something is wrong with your credentials', 401);
+
+    userServices.findUserbyEmail = jest.fn(() => {
+      throw mockError;
+    });
+
+    await authController.login(req as Request, res as Response, next);
+
+    expect(userServices.findUserbyEmail).toHaveBeenCalledWith(req.body.email);
+    expect(bcryptServices.checkPassword).not.toHaveBeenCalled();
+    expect(next).toHaveBeenCalledWith(mockError);
+  });
+
+  it('should call bcrypt.compare if user is found in the db', async () => {
+    const req: Partial<Request> = {
+      body: {
+        email: 'test@example.com',
+        password: 'password123',
+      },
+    };
+    const res: Partial<Response> = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+    const next = jest.fn();
+
+    const mockUser = {
+      email: 'test@example.com',
+      password: 'password123',
+      firstName: 'John',
+      lastName: 'Doe',
+      role: 'user',
+    };
+
+    userServices.findUserbyEmail = jest.fn().mockResolvedValue(mockUser);
+
+    await authController.login(req as Request, res as Response, next);
+
+    expect(bcryptServices.checkPassword).toHaveBeenCalledWith(req.body.password, mockUser);
+  });
+
+  it('should return a 401 error if auth fails', async () => {
+    const req: Partial<Request> = {
+      body: {
+        email: 'test@example.com',
+        password: 'password123',
+      },
+    };
+
+    const res: Partial<Response> = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+    const next = jest.fn();
+
+    const mockError = new AppError('Email does not exist', 401);
+
+    bcryptServices.checkPassword = jest.fn().mockResolvedValue(false);
+
+    await authController.login(req as Request, res as Response, next);
+
+    expect(jwtServices.sendJwtResponse).not.toHaveBeenCalled();
+    expect(next).toHaveBeenCalledWith(mockError);
   });
 });
